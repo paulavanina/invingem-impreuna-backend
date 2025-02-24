7702e5ce5af6c9e526b4a4e9c1658c3980b3d4d4
Paula <bejera_paula@yahoo.com>
2025-02-24 22:24:07 +0200

    chore: allow all origins in CORS configuration

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 app.use(
   cors({
-    origin: "*",
+    origin: true,
     credentials: true,
     methods: "GET, POST, OPTIONS, PUT, PATCH, DELETE",
   })
